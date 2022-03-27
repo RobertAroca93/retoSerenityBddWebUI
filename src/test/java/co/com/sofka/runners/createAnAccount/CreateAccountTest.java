17d34570cca80edafaf5d8c78cc4544686596679
Robert Aroca <rjaroca@corhuila.edu.co>
2022-03-26 20:23:41 -0500

    se inicia la configuracion del proyecto

---
+++ src/test/java/co/com/sofka/runners/createAnAccount/CreateAccountTest.java
@@ -0,0 +1,17 @@
+package co.com.sofka.runners.createAnAccount;
+
+
+import io.cucumber.junit.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(
+        snippets = CucumberOptions.SnippetType.CAMELCASE,
+        features = {"src/test/resources/features/createAnAccount/createAnAccount.feature"},
+        glue = {"co.com.sofka.stepDefinitions.createAnAccount"},
+        tags = {""}
+
+)
+public class CreateAccountTest {
+}
---
+++ src/test/java/co/com/sofka/stepDefinitions/createAnAccount/CreateAccountStepDefinition.java
@@ -0,0 +1,24 @@
+package co.com.sofka.stepDefinitions.createAnAccount;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class CreateAccountStepDefinition {
+
+    @Given("dado que me encuentro en el apartado de registro")
+    public void dadoQueMeEncuentroEnElApartadoDeRegistro() {
+
+    }
+
+    @When("diligencio correctamente los datos")
+    public void diligencioCorrectamenteLosDatos() {
+
+    }
+
+    @Then("el sistema debera mostrar el mensaje de bienvenida")
+    public void elSistemaDeberaMostrarElMensajeDeBienvenida() {
+
+    }
+
+}
